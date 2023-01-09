833e24b77d7bd3fa1ae025c7442b946b0d7a85f9
aho-z <990378640@qq.com>
2023-01-09 19:31:19 +0800

    复习巩固

---
+++ test_over/test_over/test.c
@@ -0,0 +1,38 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//int main() {
+//
+//	return 0;
+//}
+
+
+#include <math.h>
+int main()
+{
+	//int n;
+	//while (~scanf("%d", &n)) {
+	//	long count = 0, base = 10;
+	//	for (int i = 0; i <= n; i++) {
+	//		long pow_n = pow(i, 2);
+	//		if (i == base) base *= 10;//两位数的时候成为100，三位数的时候成为1000....
+	//		if (pow_n % base == i) count++;
+	//		/*
+	//		int tmp = i;
+	//		while(tmp){
+	//		if (tmp%10 != pow_n%10) break;
+	//		tmp/=10;
+	//		pow_n/=10;
+	//		} if
+	//		(tmp == 0)count++;
+	//		*/
+	//	}
+	//	printf("%d\n", count);
+	//} 
+	//return 0;
+
+	int k[10];
+	printf("%d\n", sizeof(&k+1));
+	return 0;
+}
