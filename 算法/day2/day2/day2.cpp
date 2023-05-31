643e6607f2b35a8790f55ecf6b9a45eaadf4d5c9
aho-z <990378640@qq.com>
2023-05-31 22:06:53 +0800

    更新

--- 算法/day2/day2/day2.cpp
+++ 算法/day2/day2/day2.cpp
@@ -65,3 +65,26 @@ int main()
     string s = "2101";
     Solution().numDecodings(s);
 }
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        //多开一行一列
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        //初始化dp表
+
+        dp[0][1] = 1;
+
+        for (int i = 1; i <= m; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m][n];
+
+    }
+};
