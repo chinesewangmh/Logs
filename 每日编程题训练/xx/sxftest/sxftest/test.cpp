af8936eb706aafa7ca8a5e72e90d2b4d5d8b9cac
aho-z <990378640@qq.com>
2023-09-15 22:11:03 +0800

    深信服

---
+++ 每日编程题训练/xx/sxftest/sxftest/test.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+#include <stdio.h>
+
+//void test(char p)
+//
+//{
+//
+//	p = "abc";
+//
+//}
+//
+//int main()
+//
+//{
+//
+//	char* p = "test";
+//	test(p);
+//	printf("%s\n", p);
+//
+//}
+
+#define MOD(X,Y) X%Y
+
+int main()
+{
+	/*int x = 67, y = 9;
+
+	printf("%d", MOD(x, y + 8) * 2);*/
+
+	int a = 7500000;
+	cout << ((a >> 16) & 255);
+
+	bool check[7];
+}
